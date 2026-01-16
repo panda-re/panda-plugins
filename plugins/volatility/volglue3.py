f1d440440c27e411f826b7220933ce8adb2dc235
Suhaas Narayan <suhaas.narayan@ll.mit.edu>
2026-01-16 11:27:00 -0500

    Benchmarking code

---
+++ plugins/volatility/volglue3.py
@@ -0,0 +1,230 @@
+# This section sets up the volatility environment
+# and is invoked by the plugin when it loads this
+# script as a module
+#
+# It should be refactored so that the setup
+# is done as a function call to make it cleaner
+# and take the profile as an argument
+import os
+import json
+import shutil
+import hashlib
+import tempfile
+import traceback
+
+# import volatility3
+from volatility3.cli import text_renderer
+from volatility3.plugins.windows import psscan, pslist, svcscan, netscan, vadinfo
+from volatility3.framework.interfaces.context import ModuleInterface, ModuleContainer
+from volatility3.framework import automagic, contexts, interfaces, plugins
+
+
+def filter_invalid_ascii(strobj):
+    return strobj.encode("utf-8", "replace")
+
+
+def is_interesting(eprocess, filter_data):
+    check_pid = int(eprocess.UniqueProcessId)
+    check_asid = int(eprocess.Pcb.DirectoryTableBase)
+
+    for (pid, tid, asid) in filter_data["threads"]:
+        if check_pid == int(pid) and check_asid == int(asid):
+            return True
+    return False
+
+
+def hash_file(filepath, *algorithms):
+    with open(filepath, "rb") as fobj:
+        while True:
+            chunk = fobj.read(4096)
+            if not chunk:
+                break
+
+            for a in algorithms:
+                a.update(chunk)
+
+    return {a.name: a.hexdigest().lower() for a in algorithms}
+
+def socket_visitor(node, accumulator):
+    if node.values:
+        proto, laddr, lport, raddr, rport, state, pid, owner = node.values[1:9]
+        sdata = {
+            "pid": pid,
+            "owner": owner,
+            "proto": proto,
+            "local_addr": laddr,
+            "local_port": lport,
+            "remote_addr": raddr,
+            "remote_port": rport,
+            "state": state,
+        }
+        accumulator.append(sdata)
+    return accumulator
+
+def pslist_visitor(node, accumulator):
+    if node.values:
+        pid, ppid, img_name, offset = node.values[0:4]
+        proc = ctx.object("symbol_table_name1!_EPROCESS", "layer_name", offset)
+        if img_name == "services.exe":
+            print(offset)
+            breakpoint()
+        sdata = {
+            "pid": int(pid),
+            "ppid": int(ppid),
+            "asid": int(proc.Pcb.DirectoryTableBase),
+            "ImagePathName": img_name,
+        }
+        accumulator.append(sdata)
+    return accumulator
+
+
+def svcscan_visitor(node, accumulator):
+    if node.values:
+        print(node)
+        breakpoint()
+    
+    return accumulator
+
+def driverscan_visitor(node, accumulator):
+    if node.values:
+        print(node)
+        offset, start = node.values[:2]
+        name = node.values[-1]
+        drv_data = {
+            "offset": int(offset),
+            "start": int(start),
+            "name": str(name),
+        }
+        accumulator.append(drv_data)
+        breakpoint()
+    
+    return accumulator
+
+
+def get_memory_hashes(filter_data):
+
+    runner = vadinfo.VADDump(config)
+
+    results = []
+    for proc in runner.calculate():
+        addr = proc.get_process_address_space()
+
+        if not addr:
+            continue
+
+        if is_interesting(proc, filter_data):
+
+            for vad, _ in proc.get_vads(
+                vad_filter=lambda v: v.Length < pow(2, 30), skip_max_commit=True
+            ):
+                path = os.path.join(
+                    config.DUMP_DIR,
+                    "{}_{}.{}".format(vad.Start, vad.End, proc.UniqueProcessId),
+                )
+
+                runner.dump_vad(path, vad, addr)
+
+                result = {
+                    "pid": int(proc.UniqueProcessId),
+                    "start": int(vad.Start),
+                    "end": int(vad.End),
+                }
+                result.update(hash_file(path, hashlib.sha256()))
+                results.append(result)
+
+    shutil.rmtree(config.DUMP_DIR)
+
+    return results
+
+
+def get_pslist():
+    """List all the tasks that aren't hidden, unlinked, etc"""
+    config_path = "plugins.PsList"
+    automagics = automagic.choose_automagic(available_automagics, pslist.PsList)
+    constructed = plugins.construct_plugin(ctx, automagics, pslist.PsList, config_path, progress_callback=None, open_method=None)
+    # breakpoint()
+    treegrid = constructed.run()
+    pslist_data = []
+    treegrid.visit(node=None, function=pslist_visitor, initial_accumulator=pslist_data)
+    return pslist_data
+    # text_renderer.PrettyTextRenderer().render(treegrid)
+
+def get_psscan():
+    """List all the tasks including hidden, unlinked, etc"""
+    config_path = "plugins.PsScan"
+    automagics = automagic.choose_automagic(available_automagics, psscan.PsScan)
+    constructed = plugins.construct_plugin(ctx, automagics, psscan.PsScan, config_path, progress_callback=None, open_method=None)
+    treegrid = constructed.run()
+    psscan_data = []
+    treegrid.visit(node=None, function=pslist_visitor, initial_accumulator=psscan_data)
+    return psscan_data
+    # text_renderer.PrettyTextRenderer().render(treegrid)
+
+def get_svcscan():
+    """List all of the system services"""
+    config_path = "plugins.SvcScan"
+    automagics = automagic.choose_automagic(available_automagics, svcscan.SvcScan)
+    constructed = plugins.construct_plugin(ctx, automagics, svcscan.SvcScan, config_path, progress_callback=None, open_method=None)
+    treegrid = constructed.run()
+    svcscan_data = []
+    treegrid.visit(node=None, function=svcscan_visitor, initial_accumulator=svcscan_data)
+    driverscan_data = get_driverscan()
+    return svcscan_data
+
+
+def get_driverscan():
+    from volatility3.plugins.windows import driverscan
+    config_path = "plugins.DriverScan"
+    automagics = automagic.choose_automagic(available_automagics, driverscan.DriverScan)
+    constructed = plugins.construct_plugin(ctx, automagics, driverscan.DriverScan, config_path, progress_callback=None, open_method=None)
+    treegrid = constructed.run()
+    driverscan_data = []
+    treegrid.visit(node=None, function=driverscan_visitor, initial_accumulator=driverscan_data)
+    return driverscan_data
+
+
+def get_sockets():
+    """List all of the sockets that have not been unlinked or hidden"""
+
+    # This only works for Vista and later
+    config_path = "plugins.NetScan"
+    automagics = automagic.choose_automagic(available_automagics, netscan.NetScan)
+    constructed = plugins.construct_plugin(ctx, automagics, netscan.NetScan, config_path, progress_callback=None, open_method=None)
+    treegrid = constructed.run()
+    socket_data = []
+    treegrid.visit(node=None, function=socket_visitor, initial_accumulator=socket_data)
+    return socket_data
+
+
+def run(location, filterfile):
+    """Returns a list of the processes as a JSON string
+
+    This analysis demonstrates that volatility can be successfully
+    invoked and that data can be serialized as JSON data and
+    returned to the plugin.
+
+    """
+    print("Volatility version: %r" % volatility3.framework.constants.VERSION)
+    try:
+        with open(filterfile, "rb") as fobj:
+            filter_data = json.load(fobj)
+
+        analysis_results = {
+            "pslist": get_pslist(),
+            "svcscan": get_svcscan(),
+            "sockets": get_sockets(),
+            "process_hashes": get_process_hashes(filter_data),
+            "memory_hashes": get_memory_hashes(filter_data),
+        }
+    except Exception as err:
+        analysis_results = {"error": traceback.format_exc(err)}
+
+    json_str = json.dumps(analysis_results, indent=1)
+    return json_str
+
+if __name__ == "__main__":
+    image = "mymem.dd"
+    ctx = contexts.Context()
+    ctx.config["automagic.LayerStacker.single_location"] = f"file:{image}"
+    available_automagics = automagic.available(ctx)
+    print(get_driverscan())
